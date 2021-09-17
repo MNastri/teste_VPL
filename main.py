3903db4c81dd4df04f7596c1aebd12d4e99cd1d3
MNastri <marcellonastri@gmail.com>
2021-09-17 15:46:24 -0300

    Solucionar a questao proposta
    - Retornar na função test o pedido pela questão. Utilizar força bruta
    permite fugir das equações financeiras.
    
    - Utilizar variaveis com dois caracteres na função test evitando erros
    de digitacao. Desta forma, utilizar a variavel rr ao inves de r e dd
    ao inves de d.
    
    - Como e possivel realizar o calculo de VPL utilizando uma taxa
    negativa na calculadora financeira, permitir que o metodo vpl da
    classe Titulo tambem faca o seu calculo com taxa negativa.
    
    - Impedir que uma taxa de -1.00 (-100%) seja utilizada no metodo vpl da classe Titulo. Essa taxa gera uma divisao por zero.
    
    - Resolver todos os casos, ou testes, com sucesso.
    
    - Melhorar impressão para a função teste.
    
    - Melhorar doc strings das funções.
    
    - Trocar tipo do parametro vencimento de str para int.
    
    - Remover comentarios de TODO.
    
    - Remover comandos que foram utilizados apenas para tirar o erro de
    lint.
    
    - Comentar código que imprime no terminal o saldo.

--- main.py
+++ main.py
@@ -19,80 +19,72 @@ a data (d) de vencimento do título.
 """
 
 
-# TODO implementar classe
 class Titulo:
     def __init__(self):
         self.remuneracao = 1_000.00
         self.liquidacao = 10_000.00
-        if self.remuneracao+self.liquidacao == 0:     # só para evitar erro de lint. EXCLUIR
-            pass
 
-    def vpl(self, taxa: float, vencimento: str) -> float:
+    def vpl(self, taxa: float, vencimento: int) -> float:
         """
         Calcular o Valor Presente Líquido do título para a taxa e vencimento.
 
         O Valor Presente Líquido de um fluxo de caixa é o saldo no presente
-        deste fluxo descapitalizado a uma taxa de juros composta.
+        deste fluxo descapitalizado a uma taxa de juros composto.
 
         :param taxa: float
             a taxa de juros que o título será descontado
-        :param vencimento: str
+        :param vencimento: int
             a data de vencimento do título
         :return: float
             retorna o valor presente do fluxo de caixa deste titulo
         """
-        if int(vencimento) <= 0 or taxa == -1.00:
-            return -1.0
-
-        # resolvendo na força bruta
+        if vencimento <= 0 or taxa == -1.00:
+            return -1.0  # erro
         rr = self.remuneracao
         saldo = self.liquidacao
-        periodos_restantes = int(vencimento)
+        periodos_restantes = vencimento
         while periodos_restantes:
             saldo = saldo / (1 + taxa)
             saldo = saldo + rr if periodos_restantes > 1 else saldo
             periodos_restantes -= 1
-            print(f'saldo = {saldo}')
-
+            print(f'saldo{periodos_restantes} = {saldo:,.2f}', end=", ")
         return saldo
 
 
-# TODO verificar se tem jeito melhor de trabalhar com datas (estou usando strings)
-# TODO implementar procedimento do teste e output no termimal
-def test(rr: float, dd: str) -> None:
+def test(rr: float, dd: int) -> float:
     """
     Realizar o teste da classe Titulo com os parâmetros dados.
 
     :param rr: float
         a taxa de juros que o título será descontado
-    :param dd: str
+    :param dd: int
         a data de vencimento do título
     :return: None
-        não retorna, apenas executa
     """
     titulo_teste = Titulo()
     output = titulo_teste.vpl(taxa=rr, vencimento=dd)
-    print(f'input:{rr} e {dd} - output:{output}')
+    print(f'input: {rr:.2f} e {dd} - output: {output:,.2f}')
+    return output
 
 
 def main_loop() -> None:
     """ Realizar todos os testes."""
-    test(0.00, '0')     # output=-1 (erro período tem que ser maior que zero)
-    test(-10.00, '0')   # output=-1 (erro período tem que ser maior que zero)
-    test(10.00, '0')    # output=-1 (erro período tem que ser maior que zero)
-    test(0.00, '-10')   # output=-1 (erro período tem que ser maior que zero)
-    test(-0.50, '-1')   # output=-1 (erro período tem que ser maior que zero)
-    test(-1.00, '1')    # output=-1 (erro taxa tem que ser diferente de -1)
+    test(0.00, 0)     # output=-1 (erro período tem que ser maior que zero)
+    test(-10.00, 0)   # output=-1 (erro período tem que ser maior que zero)
+    test(10.00, 0)    # output=-1 (erro período tem que ser maior que zero)
+    test(0.00, -10)   # output=-1 (erro período tem que ser maior que zero)
+    test(-0.50, -1)   # output=-1 (erro período tem que ser maior que zero)
+    test(-1.00, 1)    # output=-1 (erro taxa tem que ser diferente de -1)
     print('==')
-    test(0.00, '1')     # output=10_000.00  (hp12: f REG 10000 g CFj 0 i f NPV)
-    test(1.00, '1')     # output=5_000.00  (hp12: f REG 10000 g CFj 100 i f NPV)
-    test(0.01, '2')     # output=10_793.06  (hp12: f REG 1000 g CFj 10000 g CFj 1 i f NPV)
-    test(-0.01, '3')    # output=12_336.51  (hp12: f REG 1000 g CFj 2 g Nj 10000 g CFj 1 CHS i f NPV)
-    test(0.00, '3')     # output=12_000.00  (hp12: f REG 1000 g CFj 2 g Nj 10000 g CFj 0 i f NPV)
-    test(0.01, '3')     # output=11_676.30  (hp12: f REG 1000 g CFj 2 g Nj 10000 g CFj 1 i f NPV)
-    test(0.99, '3')     # output=2_023.97  (hp12: f REG 1000 g CFj 2 g Nj 10000 g CFj 99 i f NPV)
-    test(1.00, '3')     # output=2_000.00  (hp12: f REG 1000 g CFj 2 g Nj 10000 g CFj 100 i f NPV)
-    test(1.01, '3')     # output=1_976.47  (hp12: f REG 1000 g CFj 2 g Nj 10000 g CFj 101 i f NPV)
+    test(0.00, 1)     # output=10,000.00  (hp12: f REG 10000 g CFj 0 i f NPV)
+    test(1.00, 1)     # output=5,000.00  (hp12: f REG 10000 g CFj 100 i f NPV)
+    test(0.01, 2)     # output=10,793.06  (hp12: f REG 1000 g CFj 10000 g CFj 1 i f NPV)
+    test(-0.01, 3)    # output=12,336.51  (hp12: f REG 1000 g CFj 2 g Nj 10000 g CFj 1 CHS i f NPV)
+    test(0.00, 3)     # output=12,000.00  (hp12: f REG 1000 g CFj 2 g Nj 10000 g CFj 0 i f NPV)
+    test(0.01, 3)     # output=11,676.30  (hp12: f REG 1000 g CFj 2 g Nj 10000 g CFj 1 i f NPV)
+    test(0.99, 3)     # output=2,023.97  (hp12: f REG 1000 g CFj 2 g Nj 10000 g CFj 99 i f NPV)
+    test(1.00, 3)     # output=2,000.00  (hp12: f REG 1000 g CFj 2 g Nj 10000 g CFj 100 i f NPV)
+    test(1.01, 3)     # output=1,976.47  (hp12: f REG 1000 g CFj 2 g Nj 10000 g CFj 101 i f NPV)
 
 
 if __name__ == '__main__':
