7104eed9b6c3138b88f81ec929e624de4a281464
MNastri <marcellonastri@gmail.com>
2021-09-17 00:15:06 -0300

    Adicionar arquivo inicial com texto do email recebido

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+Marcelo, segue exercício para ser respondido até segunda-feira as 12:00
+O código fonte escrito deve estar no github em um repositório criado para
+este fim.
+Neste mesmo repositório deve estar a documentação que achar necessária e as
+considerações sobre a função desenvolvida.
+
+----
+
+Imagine um título de renda fixa que paga R$ 1.000 todo último dia útil do mês.
+ No vencimento do título, este título pagará R$ 10.000.
+
+Escreva uma função, em python, que retorne o valor deste título na data atual.
+
+Esta função deverá receber dois parâmetros:
+a taxa de juros (r) que o título será descontado e;
+a data (d) de vencimento do título.
+
+"""
